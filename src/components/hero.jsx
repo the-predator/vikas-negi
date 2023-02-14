70ba6eed68c0748b37a75cf6584df033edea340b
Vikas Negi <negivikas201@gmail.com>
2023-02-15 05:01:26 +0530

    2nd commit

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import '../styles.scss'
+import Fade from 'react-reveal/Fade';
 
 function Hero() {
   return (
       <section id="hero" className="jumbotron">
       <div className="container">
+        <Fade>
         <h1 className="hero-title load-hidden">
           Hi, my name is <span className="text-color-main">Vikas Negi</span>
           <br />
           I'm a Full Stack Web Developer.
         </h1>
+        
         <p className="hero-cta load-hidden">
           <a rel="noreferrer" className="cta-btn cta-btn--hero" href="#about">Know more </a>
         </p>
+        </Fade>
       </div>
     </section>
   )
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import initScrollReveal from "./scripts/scrollReveal";
-import initTiltEffect from "./scripts/tiltAnimation";
-import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<App/>);
-initScrollReveal(targetElements, defaultProps);
-initTiltEffect();
-
 
